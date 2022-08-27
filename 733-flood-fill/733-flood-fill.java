3ff233ff6f710777bfdf4c8617328615f1ede764
Vineet karmiani <vineetkarmiani@gmail.com>
2022-08-26 19:33:39 -0700

    Time: 1 ms (90.78%), Space: 48.7 MB (6.73%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -9,28 +9,22 @@ class Solution {
     public static void FloodFill(int[][] image, int sr, int sc, int color, int srcColor) 
     {
         image[sr][sc] = color;
-        boolean visited = false;
         if(sr>0 && image[sr-1][sc]==srcColor)
                 {
                     FloodFill(image,sr-1,sc, color, srcColor);
-                    visited = true;
                 }
                 if(sr<image.length-1 && image[sr+1][sc]==srcColor)
                 {
                     FloodFill(image,sr+1,sc, color, srcColor);
-                    visited = true;
                 }
                 if(sc>0 && image[sr][sc-1]==srcColor)
                 {
                     FloodFill(image,sr,sc-1, color, srcColor);
-                    visited = true;
                 }
                 if(sc<image[0].length-1 && image[sr][sc+1]==srcColor) 
                 {
                     FloodFill(image,sr,sc+1, color, srcColor);
-                    visited = true;
                 }
-        if(!visited)
-            return;
+       
     }
 }
