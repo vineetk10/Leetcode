87d07997f9d44a2efce6e839dacfa0973148e43c
Vineet karmiani <vineetkarmiani@gmail.com>
2022-09-01 20:08:55 -0700

    Time: 1 ms (89.87%), Space: 43.5 MB (44.00%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+       List<Integer> currArr = new ArrayList<>();
+        res.add(currArr);
+        for(int i=0;i<nums.length;i++)
+        {
+            Subsets(nums, res, i, currArr);
+            
+        }
+        return res;
+    }
+    
+    public static void Subsets(int[] nums, List<List<Integer>> res, int index, List<Integer> currArr)
+    {
+        if(index == nums.length)
+        {
+            res.add(new ArrayList<>(currArr));
+            return;
+        }
+        
+        for(int i = index;i<nums.length;i++)
+        {
+            
+            // Subsets(nums, res, i+1, currArr);
+            currArr.add(nums[index]);
+            Subsets(nums, res, i+1, currArr);
+            currArr.remove(currArr.size()-1);
+        }
+    }
+}
