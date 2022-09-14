6f786431402b01e798efe5c043ad234b3341e6b5
Vineet karmiani <vineetkarmiani@gmail.com>
2022-09-13 20:13:10 -0700

    Time: 8 ms (5.22%), Space: 42.2 MB (43.42%) - LeetHub

--- 274-h-index/274-h-index.java
+++ 274-h-index/274-h-index.java
@@ -3,12 +3,27 @@ class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int res = 0;
-        for(int i=citations.length-1;i>=0;i--)
-        {
-            if(citations[i]>=citations.length - i)
-                res=citations.length - i;
-        }
+        // for(int i=citations.length-1;i>=0;i--)
+        // {
+        //     if(citations[i]>=citations.length - i)
+        //         res=citations.length - i;
+        // }
+        int low = 0;
+        int high = citations.length-1;
         
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(citations[mid]>=citations.length - mid){
+                res=citations.length - mid;
+                high = mid-1;
+                
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
         return res;
     }
 }
