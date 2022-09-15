becfe8e850b149dd80f5cc71ec62038383f3a61e
Vineet karmiani <vineetkarmiani@gmail.com>
2022-09-15 10:44:08 -0700

    Time: 88 ms (43.92%), Space: 77.9 MB (61.12%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,37 +1,53 @@
 class Solution {
-    public int count = Integer.MIN_VALUE;
     public int findLength(int[] nums1, int[] nums2) {
-        // HashMap<String, Integer> hs = new HashMap<>();
-         int[][]dp = new int[nums1.length +1][nums2.length + 1];
-        for(int[] arr: dp){ 
-            for(int i =0;i <arr.length ;++i){
-                arr[i] = -1;
+        int n = nums1.length+1;
+        int m = nums2.length + 1;
+        int[][]dp = new int[n][m];
+        int max = 0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                    max = Math.max(max, dp[i][j]);
+                }
             }
         }
-        FindLength(nums1, nums2, 0 , 0, dp);
-        return count;
+        return max;
     }
+//     public int count = Integer.MIN_VALUE;
+//     public int findLength(int[] nums1, int[] nums2) {
+//          int[][]dp = new int[nums1.length +1][nums2.length + 1];
+//         for(int[] arr: dp){ 
+//             for(int i =0;i <arr.length ;++i){
+//                 arr[i] = -1;
+//             }
+//         }
+//         FindLength(nums1, nums2, 0 , 0, dp);
+//         return count;
+//     }
     
-    public int FindLength(int[] nums1, int[] nums2, int i, int j, int[][]dp)
-    {
-        if(i>=nums1.length || j>=nums2.length)
-            return 0;
+//     public int FindLength(int[] nums1, int[] nums2, int i, int j, int[][]dp)
+//     {
+//         if(i>=nums1.length || j>=nums2.length)
+//             return 0;
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
-        int c1 = 0;
+//         int c1 = 0;
         
-        if(nums1[i]==nums2[j])
-            c1 = 1+FindLength(nums1, nums2, i+1 , j+1, dp);
+//         if(nums1[i]==nums2[j])
+//             c1 = 1+FindLength(nums1, nums2, i+1 , j+1, dp);
         
         
-        count = Math.max(count, c1);
-        FindLength(nums1, nums2, i+1 , j, dp);
-        FindLength(nums1, nums2, i , j+1, dp);
+//         count = Math.max(count, c1);
+//         FindLength(nums1, nums2, i+1 , j, dp);
+//         FindLength(nums1, nums2, i , j+1, dp);
         
-        dp[i][j] = c1;
-        return c1; 
-    }
+//         dp[i][j] = c1;
+//         return c1; 
+//     }
        
 }
