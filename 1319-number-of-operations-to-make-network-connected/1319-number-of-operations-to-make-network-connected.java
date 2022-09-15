4e9c16ac73774afb7de0603fe1fce260eeedabcd
Vineet karmiani <vineetkarmiani@gmail.com>
2022-09-14 20:35:06 -0700

    Time: 91 ms (7.91%), Space: 68.3 MB (50.82%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        HashMap<Integer,List<Integer>> hm = new HashMap<>();
+        int count = 0;
+        if(connections.length<n-1)
+            return -1;
+        for(int i=0;i<connections.length;i++)
+        {
+            if(!hm.containsKey(connections[i][0]))
+                hm.put(connections[i][0], new ArrayList<Integer>());
+            
+            hm.get(connections[i][0]).add(connections[i][1]);
+            
+            if(!hm.containsKey(connections[i][1]))
+                hm.put(connections[i][1], new ArrayList<Integer>());
+            
+            hm.get(connections[i][1]).add(connections[i][0]);
+        }
+        HashSet<Integer> visited = new HashSet<>();
+        
+        for(int i=0; i < n; i++){
+            
+            if(visited.contains(i))
+                continue;
+            System.out.println(i);
+            
+            
+            DFS(hm, visited, i);
+            count++;
+        }
+        /*for(int key: hm.keySet())
+        {
+            if(visited.contains(key))
+                continue;
+            System.out.println(key);
+            count++;
+            visited.add(key);
+            DFS(hm, visited, key);
+        }*/
+        return count-1;
+    }
+    
+    public void DFS(HashMap<Integer,List<Integer>> hm, HashSet<Integer> visited, int i)
+    {
+        if(visited.contains(i))
+            return;
+        visited.add(i);
+        if(hm.containsKey(i)){
+            for(int c : hm.get(i)){
+               DFS(hm, visited, c); 
+            }
+        }
+    }
+}
